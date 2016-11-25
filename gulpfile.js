2ab82d92708ce646734539cbfd8fea2fff8ee40e
budiadiono <budiadiono@gmail.com>
2016-11-26 03:37:56 +0700

    snippets builder [gulp]

--- gulpfile.js
+++ gulpfile.js
@@ -124,6 +124,28 @@ gulp.task('build:props', function() {
 })
 
 
+gulp.task('build:snippets', function(){
+  gulp.src('./doc/api.json')
+  .pipe(through.obj(function (file, enc, cb) {
+    var json = JSON.parse(String(file.contents))
+    var results = {}
+    var res = _.each(json, function(m){      
+      results['ui-' + m.module] = {
+        prefix: 'ui-' + m.module,
+        body: '<ui-' + m.module + '>$0</ui-'+ m.module + '>',
+        "description": "VueTypedUI " + pascalCase(m.module)
+      }
+    })
+
+    file.contents = new Buffer(beautify(JSON.stringify(results)))
+    cb(null, file)
+  }))
+  .pipe(rename('snippets.json'))
+  .pipe(gulp.dest('./doc'))
+  
+})
+
+
 function parseProp(module, json) {
   var schema = JSON.parse(json)
   var strProps = ''
