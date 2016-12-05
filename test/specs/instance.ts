0a9086d12e62af6af7eb562b40c46a39ad871b53
budiadiono <budiadiono@gmail.com>
2016-12-05 08:03:28 +0700

    fix failing test

--- test/specs/instance.ts
+++ test/specs/instance.ts
@@ -4,16 +4,16 @@ import * as VueTypedUI from '../../dist/vue-typed-ui'
 import { expect } from 'chai'
 
 
-describe('vue-typed-ui instance', () => {
+describe('vue-typed-ui root instance', () => {
 
 	Vue.use(VueTypedUI)
 
-	it('should have $ui instance with $settings property', () => {
+	it('should have $UI instance with $settings property', () => {
 
 		@Component() class A extends Vue { }
 
 		var vm = (new A()).$mount()
-		expect(vm).to.have.property('$ui').to.have.property('$settings')
+		expect(vm).to.have.property('$UI').to.have.property('$settings')
 
 	})
 
