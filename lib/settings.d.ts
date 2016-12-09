d4676035e3352a1a571fafb90bc44cde98bdca99
budiadiono <budiadiono@gmail.com>
2016-12-09 11:07:47 +0700

    fix settings definitions

--- lib/settings.d.ts
+++ lib/settings.d.ts
@@ -1,18 +1,18 @@
 import { DateFormat, TimeFormat } from '../lib/types';
 
 export interface Settings {
-  dateFormat: DateFormat
-  timeFormat: TimeFormat
-  numeric: NumericSettings
+  dateFormat?: DateFormat
+  timeFormat?: TimeFormat
+  numeric?: NumericSettings
 }
 
 export interface NumericSettings {
-  groupSeparator: string
-  decimalSeparator: string
-  decimalPlaces: number
-  sign: string
-  signPos: 'left' | 'right'
-  min: number
-  max: number
+  groupSeparator?: string
+  decimalSeparator?: string
+  decimalPlaces?: number
+  sign?: string
+  signPos?: 'left' | 'right'
+  min?: number
+  max?: number
 }
 
